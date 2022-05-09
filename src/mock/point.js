cf61e3845736cfdb539a8ccb5c0ac52d8487696b
Yulya163 <suzdlik@gmail.com>
2022-05-09 16:42:08 +0400

    Унаследованы View от абстрактного класса View из фреймворка, удалены свойства и методы, которые выделены в абстрактный класс, добавлены методы для установки обработчиков событий и туда перенесен код addEventListener из Presenter

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,4 +1,4 @@
-import {getRandomInteger} from '../utils.js';
+import {getRandomInteger} from '../utils/common.js';
 
 const generatePointType = () => {
   const pointType = [
--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -1,4 +1,4 @@
-import {createElement} from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 const createFiltersTemplate = () => (
   `<form class="trip-filters" action="#" method="get">
@@ -21,21 +21,8 @@ const createFiltersTemplate = () => (
   </form>`
 );
 
-export default class FiltersView {
-  #element = null;
-
+export default class FiltersView extends AbstractView {
   get template() {
     return createFiltersTemplate();
-  }
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
   }
 }
